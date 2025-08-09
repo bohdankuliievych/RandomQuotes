720e0bf372ac556478938a618f0570c399027a5c
Bohdan Kuliievych <bkuliievych@gmail.com>
2025-08-09 14:56:41 +0300

    Using JS Modules

---
+++ quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+  {
+    author: "Steve Jobs",
+    quote: "The only way to do great work is to love what you do",
+  },
+  {
+    author: "Steve Jobs",
+    quote: "Innovation distinguishes between a leader and a follower",
+  },
+  {
+    author: "Steve Jobs",
+    quote: "Your time is limited, so don't waste it living someone else's life",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const quotes = [
-  {
-    author: "Steve Jobs",
-    quote: "The only way to do great work is to love what you do",
-  },
-  {
-    author: "Steve Jobs",
-    quote: "Innovation distinguishes between a leader and a follower",
-  },
-  {
-    author: "Steve Jobs",
-    quote: "Your time is limited, so don't waste it living someone else's life",
-  },
-];
+import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
