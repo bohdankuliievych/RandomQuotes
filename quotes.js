f26ff5eec2d4ce681dc687a2ce32c7f1a882642e
Bohdan Kuliievych <bkuliievych@gmail.com>
2025-08-10 22:38:23 +0300

    Added favourite quotes cards

--- quotes.js
+++ quotes.js
@@ -1,15 +1,75 @@
 const quotes = [
   {
-    author: "Steve Jobs",
-    quote: "The only way to do great work is to love what you do",
+    author: "Albert Einstein",
+    quote: "Imagination is more important than knowledge.",
   },
   {
-    author: "Steve Jobs",
-    quote: "Innovation distinguishes between a leader and a follower",
+    author: "Mahatma Gandhi",
+    quote: "Be the change that you wish to see in the world.",
   },
   {
-    author: "Steve Jobs",
-    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: "Nelson Mandela",
+    quote: "It always seems impossible until it's done.",
+  },
+  {
+    author: "Maya Angelou",
+    quote:
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    author: "Martin Luther King Jr.",
+    quote: "The time is always right to do what is right.",
+  },
+  {
+    author: "Winston Churchill",
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  },
+  {
+    author: "Leonardo da Vinci",
+    quote: "Learning never exhausts the mind.",
+  },
+  {
+    author: "Oscar Wilde",
+    quote: "Be yourself; everyone else is already taken.",
+  },
+  {
+    author: "Mark Twain",
+    quote: "The secret of getting ahead is getting started.",
+  },
+  {
+    author: "Eleanor Roosevelt",
+    quote: "No one can make you feel inferior without your consent.",
+  },
+  {
+    author: "Ralph Waldo Emerson",
+    quote:
+      "The only person you are destined to become is the person you decide to be.",
+  },
+  {
+    author: "Oprah Winfrey",
+    quote:
+      "The biggest adventure you can take is to live the life of your dreams.",
+  },
+  {
+    author: "Abraham Lincoln",
+    quote:
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+  },
+  {
+    author: "Marie Curie",
+    quote:
+      "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.",
+  },
+  {
+    author: "Stephen Hawking",
+    quote:
+      "However difficult life may seem, there is always something you can do and succeed at.",
+  },
+  {
+    author: "John F. Kennedy",
+    quote:
+      "Ask not what your country can do for you—ask what you can do for your country.",
   },
 ];
 
--- script.js
+++ script.js
@@ -3,16 +3,73 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
 const generateBtn = document.getElementById("generate-btn");
+const favButton = document.getElementById("fav-button");
+const svgPath = document.getElementById("Vivid-Icons");
+const favContainer = document.getElementById("fav-container");
+let quoteIndex = null;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  quoteIndex = Math.floor(Math.random() * quotes.length);
+
+  const randomQuote = quotes[quoteIndex];
   const { quote, author } = randomQuote;
+	
   quoteElement.textContent = quote;
   authorElement.textContent = author;
+
+  favButton.removeAttribute("disabled");
+
+  // if it is fav make star gold
+  if (randomQuote.isFavourite) {
+    svgPath.setAttribute("fill", "#ffd732");
+    return;
+  }
+  // if not keep star outlined
+  svgPath.setAttribute("fill", "none");
+}
+
+function toggleFavourite() {
+  const currentQuote = quotes[quoteIndex];
+  if (quoteIndex === null) {
+    return;
+  }
+  // if is not fav
+  if (!currentQuote.isFavourite) {
+    currentQuote.isFavourite = "true";
+    svgPath.setAttribute("fill", "#ffd732");
+    // add card with favourite quote
+    const favCard = document.createElement("div");
+    favCard.classList.add("favourite-card");
+    favCard.classList.add("card");
+    favCard.style.width = "18rem";
+
+    const favCardBody = document.createElement("div");
+    favCardBody.classList.add("card-body");
+    favCard.appendChild(favCardBody);
+
+    favCardBody.innerHTML = `<p>${currentQuote.quote}</p>
+		<p class="author">${currentQuote.author}</p>`;
+    favContainer.appendChild(favCard);
+    return;
+  }
+  // if it is fav
+  currentQuote.isFavourite = !currentQuote.isFavourite;
+  svgPath.setAttribute("fill", "none");
+
+  const cardBodies = document.querySelectorAll(".card-body");
+  cardBodies.forEach((cardBody) => {
+    if (cardBody.textContent.includes(currentQuote.quote)) {
+      const card = cardBody.closest(".card");
+      // If a card is found, remove it from the DOM
+      if (card) {
+        card.remove();
+      }
+    }
+  });
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
+favButton.addEventListener("click", toggleFavourite);
 
 // theme, theme button
 const body = document.getElementsByTagName("body")[0];
