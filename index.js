4f59b2de63935a5c041d170d1f89d1ff9b16657d
Bohdan Kuliievych <bkuliievych@gmail.com>
2025-11-20 21:02:19 +0200

    save current and favourites in localStorage

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ import {
   handleFavBtnColor,
   toggleQuote,
 } from "./src/favBtn.js";
+import { addFavCard } from "./src/favCard.js";
 import { chooseRandomQuote, renderQuote } from "./src/generateBtn.js";
 import { quotes } from "./src/data/quotes.js";
+import {
+  setLocalItem,
+  getLocalItem,
+  removeLocalItem,
+  clearLocalStorage,
+} from "./src/utils.js";
 
 const favContainer = document.getElementById("fav-container");
 const favButton = document.getElementById("fav-button");
@@ -15,19 +22,76 @@ const themeButton = document.getElementById("theme-button");
 const generateBtn = document.getElementById("generate-btn");
 
 let currentQuote = null;
-
-disableFavBtn(favButton);
+const favouriteQuotes = [];
 
 function generateQuoteHandler() {
   enableFavBtn(favButton, quotePara);
   currentQuote = chooseRandomQuote();
   const isFav = renderQuote(currentQuote);
   handleFavBtnColor(isFav);
+  setLocalItem("currentQuote", currentQuote);
+}
+function updateQuotesArray(favArray, srcArray) {
+  favArray.forEach((favQuote) => {
+    let foundQuote = srcArray.find((quote) => quote.id === favQuote.id);
+    if (foundQuote) {
+      foundQuote.isFavourite = true;
+    }
+  });
 }
 
+function updateFavouriteQuotes(quote) {
+  const foundIndex = favouriteQuotes.findIndex(
+    (favQuote) => favQuote.id === quote.id
+  );
+  if (quote.isFavourite) {
+    if (foundIndex === -1) {
+      favouriteQuotes.push(quote);
+    }
+  }
+  if (!quote.isFavourite) {
+    if (foundIndex !== -1) {
+      favouriteQuotes.splice(foundIndex, 1);
+    }
+  }
+}
+function init() {
+  // local storage current quote
+  const localQuote = getLocalItem("currentQuote");
+  if (localQuote) {
+    favButton.removeAttribute("disabled");
+    const isFav = renderQuote(localQuote);
+    handleFavBtnColor(isFav);
+    currentQuote = quotes.find((quote) => quote.id === localQuote.id);
+    currentQuote.isFavourite = localQuote.isFavourite;
+  }
+  // local storage favourite quotes
+  const localQuotes = getLocalItem("favouriteQuotes");
+  if (localQuotes) {
+    localQuotes.forEach((item) => {
+      favouriteQuotes.push(item);
+    });
+  }
+  if (favouriteQuotes.length > 0) {
+    // render favourites
+    favouriteQuotes.forEach((element) => {
+      addFavCard(element, favContainer);
+    });
+    updateQuotesArray(favouriteQuotes, quotes);
+  }
+}
+
+disableFavBtn(favButton);
+
+window.addEventListener("load", init);
+
 generateBtn.addEventListener("click", generateQuoteHandler);
+
 favButton.addEventListener("click", () => {
   toggleQuote(currentQuote, favContainer);
+  setLocalItem("currentQuote", currentQuote);
+  updateFavouriteQuotes(currentQuote);
+  setLocalItem("favouriteQuotes", favouriteQuotes);
 });
 
 themeButton.addEventListener("click", () => {
@@ -45,4 +109,7 @@ favContainer.addEventListener("click", (e) => {
     favContainer
   );
   handleFavBtnColor(currentQuote.isFavourite);
+
+  updateFavouriteQuotes(quotes.find((q) => q.id === cardId));
+  setLocalItem("favouriteQuotes", favouriteQuotes);
 });
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,40 @@
 function generateRandomInt(num) {
   return Math.floor(Math.random() * num);
 }
-export { generateRandomInt };
+// local storage
+function setLocalItem(key, value) {
+  if (typeof key !== "string") {
+    console.error("Invalid key");
+    return;
+  }
+  try {
+    const jsonVal = JSON.stringify(value);
+    localStorage.setItem(key, jsonVal);
+  } catch (error) {
+    console.error("An error occcured", error);
+  }
+}
+
+function getLocalItem(key) {
+  let value = localStorage.getItem(key);
+  try {
+    return JSON.parse(value);
+  } catch {
+    return value;
+  }
+}
+
+function removeLocalItem(key) {
+  localStorage.removeItem(key);
+}
+
+function clearLocalStorage() {
+  localStorage.clear();
+}
+export {
+  generateRandomInt,
+  setLocalItem,
+  getLocalItem,
+  removeLocalItem,
+  clearLocalStorage,
+};
